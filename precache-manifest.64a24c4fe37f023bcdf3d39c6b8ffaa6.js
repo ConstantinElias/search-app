bbfceb23e6b51d05209802ed1db8e6d19611c9ff
ConstantinElias <constantine.elias@hotmail.com>
2019-10-22 13:29:16 +0300

    Updates

---
+++ precache-manifest.64a24c4fe37f023bcdf3d39c6b8ffaa6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b34dfe9ece7e09354c274f0dd224ed",
+    "url": "/xe-app/index.html"
+  },
+  {
+    "revision": "df22fae2a38b20cd3881",
+    "url": "/xe-app/static/css/2.e3fbfae2.chunk.css"
+  },
+  {
+    "revision": "b51b257700ba604b5268",
+    "url": "/xe-app/static/css/main.0f450ad8.chunk.css"
+  },
+  {
+    "revision": "df22fae2a38b20cd3881",
+    "url": "/xe-app/static/js/2.9d5a127f.chunk.js"
+  },
+  {
+    "revision": "b51b257700ba604b5268",
+    "url": "/xe-app/static/js/main.0b1ef88c.chunk.js"
+  },
+  {
+    "revision": "fccdd2a5276ba8e6f79c",
+    "url": "/xe-app/static/js/runtime-main.c4f8c98d.js"
+  },
+  {
+    "revision": "38f20377187a3d4fbd1f1d22d693d289",
+    "url": "/xe-app/static/media/logo.38f20377.png"
+  },
+  {
+    "revision": "a6f1e736e68fbaa2e79da76128b92386",
+    "url": "/xe-app/static/media/search.a6f1e736.png"
+  }
+]);
